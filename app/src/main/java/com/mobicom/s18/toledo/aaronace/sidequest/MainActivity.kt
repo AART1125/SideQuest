d550af9b24d1c68965023f078393d01e09a9b023
chiarajugno <jugnochiara@gmail.com>
2025-06-19 10:58:35 +0800

    test commit

--- app/src/main/java/com/mobicom/s18/toledo/aaronace/sidequest/MainActivity.kt
+++ app/src/main/java/com/mobicom/s18/toledo/aaronace/sidequest/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SideQuestTheme {
-        Greeting("Android")
+        Greeting("test")
     }
 }
